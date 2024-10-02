6df1228fca62077b63f07182102e2c4cfba7d7eb
Drew Igoe <drewigoe@gmail.com>
2024-10-02 11:32:04 -0700

    Initial Commit

---
+++ Workout/WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutApp.swift
+//  Workout
+//
+//  Created by Drew Igoe on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
